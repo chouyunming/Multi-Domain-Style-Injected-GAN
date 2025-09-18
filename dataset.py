b62151f296e6b2d7fe23609d9d515d212316a368
chouyunming <44692942+chouyunming@users.noreply.github.com>
2025-09-18 21:59:17 +0800

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -5,7 +5,6 @@ from PIL import Image
 from torch.utils.data import Dataset
 from torchvision import transforms
 
-
 class MultiDomainStyleTransferDataset(Dataset):
     """
     Multi-domain dataset for StyleCycleGAN.
@@ -92,7 +91,6 @@ class MultiDomainStyleTransferDataset(Dataset):
         return max(len(self.source_files), 
                   max(len(files) for files in self.target_files_by_domain.values()))
 
-
 class InferenceDataset(Dataset):
     """
     Dataset for inference - loads images from a single directory.
@@ -131,45 +129,5 @@ class InferenceDataset(Dataset):
         return image_tensor, filename
     
     def __len__(self):
-        return len(self.image_files)
 
-
-class InferenceDataset(Dataset):
-    """
-    Dataset for inference - loads images from a single directory.
-    """
-    def __init__(self, input_dir, image_size):
-        self.image_size = image_size
-        self.transform = transforms.Compose([
-            transforms.Resize((image_size, image_size)),
-            transforms.ToTensor(),
-            transforms.Normalize((0.5,) * 3, (0.5,) * 3)
-        ])
-        
-        # Get all image files from input directory
-        self.image_files = self._get_image_files(input_dir)
-        print(f"Found {len(self.image_files)} images for inference in {input_dir}")
-    
-    def _get_image_files(self, directory):
-        """Get all image files from a directory."""
-        if not os.path.exists(directory):
-            print(f"Warning: Directory {directory} does not exist")
-            return []
-            
-        image_extensions = ['*.jpg', '*.jpeg', '*.png', '*.JPG', '*.JPEG', '*.PNG']
-        files = []
-        for ext in image_extensions:
-            files.extend(glob.glob(os.path.join(directory, ext)))
-        return sorted(files)
-    
-    def __getitem__(self, index):
-        file_path = self.image_files[index]
-        image = Image.open(file_path).convert('RGB')
-        image_tensor = self.transform(image)
-        
-        # Return tensor and filename
-        filename = os.path.basename(file_path)
-        return image_tensor, filename
-    
-    def __len__(self):
         return len(self.image_files)

16d2c77a860f696d6aa744bef326c8a38235cd34
AlanKung <a0906865088@gmail.com>
2025-09-19 09:58:06 +0000

    Added: add new comment

--- config.py
+++ config.py
@@ -2,6 +2,7 @@
 # ===================================================================
 # Multi-Domain Style-Injected GAN Training Settings
 # ===================================================================
+# I'm Alan
 
 # Data paths
 SOURCE_DIR = "./data/src/Tomato_Healthy"  # Single source domain
